041fcc8518cc28079ab205b1f59a70c1345ea3c5
astro-dancer-67 <76273622+astro-dancer-67@users.noreply.github.com>
2020-12-22 14:52:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-// Get Localstorage As Storage
-var storage = window.localStorage;
-console.log(storage);
-storage.clear();
-storage.setItem("name", "Ehan Muurtaza");
-storage.setItem("age", "30");
-storage.setItem("email", "ehan@gmail.com");
-console.log(storage.getItem("age"));
+// Initalize our app
+firebase.initalizeApp({
+  apiKey: "AIzaSyAv3TPG0rtCa1p7sd3Tp607kWOYVvn_NAA",
+  authDomain: "webimals-us.firebaseapp.com",
+  databaseURL: "https://webimals-us-default-rtdb.firebaseio.com",
+  projectId: "webimals-us",
+  storageBucket: "webimals-us.appspot.com",
+  messagingSenderId: "291693058015",
+  appId: "1:291693058015:web:99ae9d69914bca64a6966a",
+  measurementId: "G-WPRW2MCELB"
+});
+
+// Add Firstore
+const firestore = firebase.firestore();
