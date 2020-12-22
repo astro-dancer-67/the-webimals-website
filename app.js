69037b4acb6a73ab929ae8685da010b89fe72c01
astro-dancer-67 <76273622+astro-dancer-67@users.noreply.github.com>
2020-12-22 09:57:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ var storage = window.localStorage;
 console.log(storage);
 storage.clear();
 storage.setItem("name", "Ehan Muurtaza");
+storage.setItem("age", "30");
+storage.setItem("email", "ehan@gmail.com");
+console.log(storage.getItem("age"));
